c42bf3125d530efb9089569b3926e374b7a6010d
hgoegheo <morimori2003@gmail.dom>
2020-10-05 15:23:38 +0900

    ハッキングの項目を増やした
    なんかmacとwindowsですれ違ってるねんけど

---
+++ Assets/game/Camera/camera_angle.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class camera_angle : MonoBehaviour
+{
+    //camera_collision CCscript;
+    //GameObject camera_hanntei;
+    public camera_collision cameraC;
+    string CCscript;
+
+    void Start()
+    {
+        //camera_hanntei = GameObject.Find("camera_hanntei");
+        //CCscript = camera_hanntei.GetComponent<camera_collision>();
+
+        CinemachineVirtualCamera CVcamera = this.gameObject.GetComponent<CinemachineVirtualCamera>();
+
+        CCscript = cameraC.nani_hureta;
+    }
+
+
+    void Update()
+    {
+        if (CCscript == "hall_in")
+        {
+            //CVcamera.Priority = 40;
+            Debug.Log("Hello");
+        }
+        if (CCscript == "hall_out")
+        {
+            Debug.Log("HHello");
+        }
+
+        if (CCscript == "tuuro_in")
+        {
+            Debug.Log("HHHello");
+        }
+        if (CCscript == "tuuro_out")
+        {
+            Debug.Log("HHHHello");
+        }
+    }
+}
